d462b53008b2525ad96f0ecb24acf4227cdfebbf
dr2moscow <dr2moscow@gmail.com>
2021-11-21 21:14:52 +0300

    ДЗ 3. Client - переименование функций, создание класса

--- client.py
+++ client.py
@@ -1,41 +1,70 @@
 """Программа-клиент"""
-
 import sys
 import json
 import socket
 import time
-from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, \
+    ERROR
 from common.utils import get_message, send_message
 
 
-def create_presence(account_name='Guest'):
-    """
-    Функция генерирует запрос о присутствии клиента
-    :param account_name:
-    :return:
-    """
-    out = {
-        ACTION: PRESENCE,
-        TIME: time.time(),
-        USER: {
-            ACCOUNT_NAME: account_name
+class Client:
+    def __init__(self, server_address, server_port):
+        self.address = server_address
+        self.port = server_port
+        try:
+            # Инициализация сокета и обмен
+            self.status = "login"
+            self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.transport.connect((self.address, self.port))
+        except BaseException:
+            print("Ошибка!")
+            sys.exit(1)
+
+    @staticmethod
+    def status_presence(account_name='Guest'):
+        """
+        Функция генерирует запрос о присутствии клиента
+        :param account_name:
+        :return:
+        """
+        out = {
+            ACTION: PRESENCE,
+            TIME: time.time(),
+            USER: {
+                ACCOUNT_NAME: account_name
+            }
         }
-    }
-    return out
+        return out
 
+    @staticmethod
+    def process_answer(message):
+        """
+        Функция разбирает ответ сервера
+        :param message:
+        :return:
+        """
+        if RESPONSE in message:
+            if message[RESPONSE] == 200:
+                return '200 : OK'
+            return f'400 : {message[ERROR]}'
+        raise ValueError
 
-def process_ans(message):
-    """
-    Функция разбирает ответ сервера
-    :param message:
-    :return:
-    """
-    if RESPONSE in message:
-        if message[RESPONSE] == 200:
-            return '200 : OK'
-        return f'400 : {message[ERROR]}'
-    raise ValueError
+    def message_send(self, message):
+        """
+        Функция по отправка сообщения
+        :param message: тело сообщения
+        """
+        if message == PRESENCE:
+            message_to_server = self.status_presence()
+        else:
+            sys.exit(0)
+        send_message(self.transport, message_to_server)
+        try:
+            answer = self.process_answer(get_message(self.transport))
+            print(answer)
+        except (ValueError, json.JSONDecodeError):
+            print(MESS_DECODE_ERROR)
 
 
 def main():
@@ -54,18 +83,10 @@ def main():
         print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
         sys.exit(1)
 
-    # Инициализация сокета и обмен
-
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server)
-    try:
-        answer = process_ans(get_message(transport))
-        print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
+    # Создадем клиент
+    client_connect = Client(server_address, server_port)
+    client_connect.message_send(PRESENCE)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
