83b52a83cd5378240af07f8d30a50fe091639cbe
dr2moscow <dr2moscow@gmail.com>
2021-11-27 18:25:40 +0300

    ДЗ 4. Ошибка, тест не может найти функцию

--- unit_tests/test_server.py
+++ unit_tests/test_server.py
@@ -1,9 +1,27 @@
-from unittest import TestCase, main
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+from common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE
+from server import process_client_message
 
 
-class Server(TestCase):
-    pass
+class TestServer(unittest.TestCase):
+    err_dict = {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+    ok_dict = {RESPONSE: 200}
+
+    """
+        Тест для класса Server, функции process_client_message
+    """
+
+    def test_ok_check(self):
+        self.assertEqual(server.process_client_message(
+            {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.ok_dict)
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
