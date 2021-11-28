ee6a27b6520090d1dc7a9fe7c72064797f8184d4
dr2moscow <dr2moscow@gmail.com>
2021-11-28 19:12:59 +0300

    ДЗ 4. Победил импорт класса

--- unit_tests/test_server.py
+++ unit_tests/test_server.py
@@ -4,7 +4,9 @@ import sys
 
 sys.path.append(os.path.join(os.getcwd(), '..'))
 from common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE
-from server import process_client_message
+import server
+
+class_server = server.Server.process_client_message
 
 
 class TestServer(unittest.TestCase):
@@ -15,12 +17,38 @@ class TestServer(unittest.TestCase):
     ok_dict = {RESPONSE: 200}
 
     """
-        Тест для класса Server, функции process_client_message
+    Тест для класса Server, функции process_client_message
     """
 
     def test_ok_check(self):
-        self.assertEqual(server.process_client_message(
+        """Корректный запрос"""
+        self.assertEqual(class_server(
             {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.ok_dict)
+
+    def test_no_action(self):
+        """Ошибка если нет действия"""
+        self.assertEqual(server.Server.process_client_message(
+            {TIME: '1.1', USER: {ACCOUNT_NAME: 'Guest'}}), self.err_dict)
+
+    def test_wrong_action(self):
+        """Ошибка если неизвестное действие"""
+        self.assertEqual(server.Server.process_client_message(
+            {ACTION: 'Wrong', TIME: '1.1', USER: {ACCOUNT_NAME: 'Guest'}}), self.err_dict)
+
+    def test_no_time(self):
+        """Ошибка, если  запрос не содержит штампа времени"""
+        self.assertEqual(server.Server.process_client_message(
+            {ACTION: PRESENCE, USER: {ACCOUNT_NAME: 'Guest'}}), self.err_dict)
+
+    def test_no_user(self):
+        """Ошибка - нет пользователя"""
+        self.assertEqual(server.Server.process_client_message(
+            {ACTION: PRESENCE, TIME: '1.1'}), self.err_dict)
+
+    def test_unknown_user(self):
+        """Ошибка - не Guest"""
+        self.assertEqual(class_server(
+            {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest1'}}), self.err_dict)
 
 
 if __name__ == '__main__':
