ead5460371a7ae71f07e62e3c779e461a57ec6d0
dr2moscow <dr2moscow@gmail.com>
2021-11-29 20:56:05 +0300

    ДЗ 5. Логирование добавлено в client.py

--- client.py
+++ client.py
@@ -12,9 +12,16 @@ import sys
 import json
 import socket
 import time
-from common.variables import DEFAULT_IP_ADDRESS, DEFAULT_PORT, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, RESPONSE, \
-    ERROR
+import argparse
+import logging
+import logs.config_client_log
+from errors import ReqFieldMissingError
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, DEFAULT_PORT, ERROR, DEFAULT_IP_ADDRESS
 from common.utils import get_message, send_message
+
+# Инициализация клогера
+logger = logging.getLogger('client')
 
 
 class Client:
@@ -25,11 +32,21 @@ class Client:
             # Инициализация сокета и обмен
             self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.transport.connect((self.address, self.port))
-        except BaseException:
-            print("Ошибка!")
-            sys.exit(1)
+            message_to_server = Client.status_presence()
+            send_message(self.transport, message_to_server)
+            answer = Client.process_answer(get_message(self.transport))
+            logger.info(f'Принят ответ от сервера {answer}')
+            print(answer)
+        except json.JSONDecodeError:
+            logger.error(f'Не удалось декодировать полученную Json строку.')
+        except ConnectionRefusedError:
+            logger.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                            f'конечный компьютер отверг запрос на подключение.')
+        except ReqFieldMissingError as missing_error:
+            logger.error(f'В ответе сервера отсутствует необходимое поле '
+                         f'{missing_error.missing_field}')
+        sys.exit(1)
 
-    @staticmethod
     def status_presence(account_name='Guest'):
         """
         Функция генерирует запрос о присутствии клиента
@@ -43,73 +60,56 @@ class Client:
                 ACCOUNT_NAME: account_name
             }
         }
+        logger.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
         return out
 
-    @staticmethod
     def process_answer(message):
         """
         Функция разбирает ответ сервера
         :param message:
         :return:
         """
+        logger.debug(f'Разбор сообщения от сервера: {message}')
         if RESPONSE in message:
             if message[RESPONSE] == 200:
                 return '200 : OK'
             return f'400 : {message[ERROR]}'
-        raise ValueError
+        raise ReqFieldMissingError(RESPONSE)
 
-    def message_send(self, message):
+    @staticmethod
+    def create_arg_parser():
         """
-        Функция по отправка сообщения
-        :param message: тело сообщения
+        Создаём парсер аргументов коммандной строки
+        :return:
         """
-        if message == PRESENCE:
-            message_to_server = self.status_presence()
-        else:
-            sys.exit(0)
-        send_message(self.transport, message_to_server)
-        try:
-            answer = self.process_answer(get_message(self.transport))
-            print(answer)
-        except (ValueError, json.JSONDecodeError):
-            print(MESS_DECODE_ERROR)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+        parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+        return parser
 
 
 def main():
     """
     Загружаем параметы коммандной строки
     """
+    parser = Client.create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
 
-    # Загружаем на какой порт сервера слать сообщение
-    try:
-        if '-p' in sys.argv:
-            server_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            server_port = DEFAULT_PORT
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        print(
-            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+    # проверим подходящий номер порта
+    if not 1023 < server_port < 65536:
+        logger.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
+            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
-    # Загружаем на какой адрес сервера слать сообщение
-    try:
-        if '-a' in sys.argv:
-            server_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            server_address = DEFAULT_IP_ADDRESS
-    except IndexError:
-        print(
-            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
-        sys.exit(1)
+    logger.info(f'Запущен клиент с парамертами: адрес сервера: '
+                f'{server_address} , порт: {server_port}')
 
     # Создадем клиент
     client_connect = Client(server_address, server_port)
-    client_connect.message_send(PRESENCE)
+    client_connect.message_send(Client.status_presence)
 
 
 if __name__ == "__main__":
